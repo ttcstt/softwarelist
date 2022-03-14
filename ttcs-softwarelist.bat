d7f8079e258d30630c5617a63fcd3c0570694181
ttcstt <90714958+ttcstt@users.noreply.github.com>
2022-03-14 10:07:39 -0400

    firefox to 98.0.1 and joplin to 2.7.14

--- ttcs-softwarelist.bat
+++ ttcs-softwarelist.bat
@@ -1,4 +1,4 @@
-set ver.firefox=98.0
+set ver.firefox=98.0.1
 set exe.firefox=Firefox Setup %ver.firefox%.exe
 set url.firefox="https://download.mozilla.org/?product=firefox-%ver.firefox%-ssl&os=win64&lang=en-US"
 set arg.firefox=-ms
@@ -33,7 +33,7 @@ set url.vlc=https://get.videolan.org/vlc/%ver.vlc%/win64/%exe.vlc%
 set arg.vlc=/S
 set chk.vlc='%ProgramFiles%\VideoLAN\VLC\vlc.exe'
 
-set ver.joplin=2.7.13
+set ver.joplin=2.7.14
 set exe.joplin=Joplin-Setup-%ver.joplin%.exe
 set url.joplin=https://github.com/laurent22/joplin/releases/download/v%ver.joplin%/%exe.joplin%
 set arg.joplin=/ALLUSERS=1 /S
