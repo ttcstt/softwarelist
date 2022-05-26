80518c4241f69d664590b04fe288c2b6e8de6d27
ttcstt <90714958+ttcstt@users.noreply.github.com>
2022-05-26 15:14:18 -0400

    Update ttcs-softwarelist.bat

--- ttcs-softwarelist.bat
+++ ttcs-softwarelist.bat
@@ -9,6 +9,7 @@ set exe.firefox=Firefox Setup %pkgver.firefox%.exe
 set url.firefox="https://download.mozilla.org/?product=firefox-%pkgver.firefox%-ssl&os=win64&lang=en-US"
 set arg.firefox=-ms
 set chk.firefox=%ProgramFiles%\Mozilla Firefox\firefox.exe
+set regtext.firefox=firefox 
 set reg.firefox=%uninstallreg64%\Mozilla Firefox %pkgver.firefox% (x64 en-US)
 
 set pkgver.loffice=7.3.3
@@ -18,6 +19,7 @@ set exe.loffice=LibreOffice_%pkgver.loffice%_Win_x64.msi
 set url.loffice=https://download.documentfoundation.org/libreoffice/stable/%pkgver.loffice%/win/x86_64/%exe.loffice%
 set arg.loffice=/qn /passive /norestart
 set chk.loffice=%ProgramFiles%\LibreOffice\program\soffice.exe
+set regtext.loffice=(multilanguage)
 set reg.loffice=%uninstallreg64%\{2C4A93B5-0849-44CC-8302-C73E05930E93}
 
 set pkgver.lofficehelp=7.3.3
@@ -27,6 +29,7 @@ set exe.lofficehelp=LibreOffice_%pkgver.lofficehelp%_Win_x64_helppack_en-US.msi
 set url.lofficehelp=https://download.documentfoundation.org/libreoffice/stable/%pkgver.lofficehelp%/win/x86_64/%exe.lofficehelp%
 set arg.lofficehelp=/qn /passive /norestart
 set chk.lofficehelp=%ProgramFiles%\LibreOffice\help\en-US\text\shared\need_help.html
+set regtext.lofficehelp=Help Pack
 set reg.lofficehelp=%uninstallreg64%\{DEB65815-C9CF-402C-9608-28F5A212B610}
 
 set pkgver.pdfsam=4.3.0
@@ -36,6 +39,7 @@ set exe.pdfsam=pdfsam-%pkgver.pdfsam%.msi
 set url.pdfsam=https://github.com/torakiki/pdfsam/releases/download/v%pkgver.pdfsam%/%exe.pdfsam%
 set arg.pdfsam=/quiet /passive CHECK_FOR_UPDATES=false SKIPTHANKSPAGE=Yes
 set chk.pdfsam=%ProgramFiles%\PDFsam Basic\pdfsam.exe
+set regtext.pdfsam=PDFsam Basic
 set reg.pdfsam=%uninstallreg64%\{8BD7882E-AF21-4327-9A48-333CAD22D3A1}
 
 set pkgver.npp=8.4.1
@@ -45,6 +49,7 @@ set exe.npp=npp.%ver.npp%.Installer.x64.exe
 set url.npp=https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v%pkgver.npp%/%exe.npp%
 set arg.npp=/S
 set chk.npp=%ProgramFiles%\Notepad++\notepad++.exe
+set regtext.npp=Notepad++ (64-bit x64)
 set reg.npp=%uninstallreg64%\Notepad++
 
 set pkgver.vlc=3.0.17.4
@@ -54,6 +59,7 @@ set exe.vlc=vlc-%pkgver.vlc%-win64.exe
 set url.vlc=https://get.videolan.org/vlc/%pkgver.vlc%/win64/%exe.vlc%
 set arg.vlc=/S
 set chk.vlc=%ProgramFiles%\VideoLAN\VLC\vlc.exe
+set reg.vlc=VLC media player
 set reg.vlc=%uninstallreg64%\VLC media player
 
 set pkgver.joplin=2.7.15
@@ -63,6 +69,7 @@ set exe.joplin=Joplin-Setup-%pkgver.joplin%.exe
 set url.joplin=https://github.com/laurent22/joplin/releases/download/v%pkgver.joplin%/%exe.joplin%
 set arg.joplin=/ALLUSERS=1 /S
 set chk.joplin=%ProgramFiles%\Joplin\Joplin.exe
+set regtext.joplin=Joplin
 set reg.joplin=%uninstallreg64%\499c114e-8890-5040-9c02-24abe7d3ebe9
 
 set pkgver.bbit=4.4.2
@@ -72,6 +79,7 @@ set exe.bbit=BleachBit-%pkgver.bbit%-setup.exe
 set url.bbit=https://download.bleachbit.org/%exe.bbit%
 set arg.bbit=/S /allusers
 set chk.bbit=%ProgramFiles(x86)%\BleachBit\bleachbit.exe
+set regtext.bbit
 set reg.bbit=%uninstallreg32%\BleachBit
 
 set pkgver.7zip=2107
@@ -112,7 +120,7 @@ set reg.tstamps=%uninstallreg32%\Tux Paint Stamps_is1
 
 set pkgver.puzzlecollection=20220128.c43a34f
 set ver.puzzlecollection=0.0.13020.0
-set name.puzzlecollection=Simon's Tatham's Portable Puzzle Collection
+set name.puzzlecollection=Simon Tatham's Portable Puzzle Collection
 set exe.puzzlecollection=puzzles-%pkgver.puzzlecollection%-installer.msi
 set url.puzzlecollection=https://www.chiark.greenend.org.uk/~sgtatham/puzzles/%exe.puzzlecollection%
 set arg.puzzlecollection=/qn /norestart
