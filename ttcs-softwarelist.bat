37062f6bf030e281e60176156e67e3cfd8fc7dba
ttcstt <90714958+ttcstt@users.noreply.github.com>
2022-05-26 23:13:28 -0400

    Update ttcs-softwarelist.bat

--- ttcs-softwarelist.bat
+++ ttcs-softwarelist.bat
@@ -9,8 +9,9 @@ set exe.firefox=Firefox Setup %pkgver.firefox%.exe
 set url.firefox="https://download.mozilla.org/?product=firefox-%pkgver.firefox%-ssl&os=win64&lang=en-US"
 set arg.firefox=-ms
 set chk.firefox=%ProgramFiles%\Mozilla Firefox\firefox.exe
-set regtext.firefox=firefox 
-set reg.firefox=%uninstallreg64%\Mozilla Firefox %pkgver.firefox% (x64 en-US)
+set regtext.firefox=firefox
+set regsearch.firefox=%uninstallreg64%
+set reg.firefox=Mozilla Firefox %pkgver.firefox% (x64 en-US)
 
 set pkgver.loffice=7.3.3
 set ver.loffice=7.3.3.2
@@ -20,7 +21,8 @@ set url.loffice=https://download.documentfoundation.org/libreoffice/stable/%pkgv
 set arg.loffice=/qn /passive /norestart
 set chk.loffice=%ProgramFiles%\LibreOffice\program\soffice.exe
 set regtext.loffice=(multilanguage)
-set reg.loffice=%uninstallreg64%\{2C4A93B5-0849-44CC-8302-C73E05930E93}
+set regsearch.loffice=%uninstallreg64%
+set reg.loffice={2C4A93B5-0849-44CC-8302-C73E05930E93}
 
 set pkgver.lofficehelp=7.3.3
 set ver.lofficehelp=7.3.3.2
@@ -30,7 +32,8 @@ set url.lofficehelp=https://download.documentfoundation.org/libreoffice/stable/%
 set arg.lofficehelp=/qn /passive /norestart
 set chk.lofficehelp=%ProgramFiles%\LibreOffice\help\en-US\text\shared\need_help.html
 set regtext.lofficehelp=Help Pack
-set reg.lofficehelp=%uninstallreg64%\{DEB65815-C9CF-402C-9608-28F5A212B610}
+set regsearch.lofficehelp=%uninstallreg64%
+set reg.lofficehelp={DEB65815-C9CF-402C-9608-28F5A212B610}
 
 set pkgver.pdfsam=4.3.0
 set ver.pdfsam=4.3.0.0
@@ -40,7 +43,8 @@ set url.pdfsam=https://github.com/torakiki/pdfsam/releases/download/v%pkgver.pdf
 set arg.pdfsam=/quiet /passive CHECK_FOR_UPDATES=false SKIPTHANKSPAGE=Yes
 set chk.pdfsam=%ProgramFiles%\PDFsam Basic\pdfsam.exe
 set regtext.pdfsam=PDFsam Basic
-set reg.pdfsam=%uninstallreg64%\{8BD7882E-AF21-4327-9A48-333CAD22D3A1}
+set regsearch.pdfsam=%uninstallreg64%
+set reg.pdfsam={8BD7882E-AF21-4327-9A48-333CAD22D3A1}
 
 set pkgver.npp=8.4.1
 set ver.npp=%pkgver.npp%
@@ -50,7 +54,8 @@ set url.npp=https://github.com/notepad-plus-plus/notepad-plus-plus/releases/down
 set arg.npp=/S
 set chk.npp=%ProgramFiles%\Notepad++\notepad++.exe
 set regtext.npp=Notepad++ (64-bit x64)
-set reg.npp=%uninstallreg64%\Notepad++
+set regsearch.npp=%uninstallreg64%
+set reg.npp=Notepad++
 
 set pkgver.vlc=3.0.17.4
 set ver.vlc=%pkgver.vlc%
@@ -59,8 +64,9 @@ set exe.vlc=vlc-%pkgver.vlc%-win64.exe
 set url.vlc=https://get.videolan.org/vlc/%pkgver.vlc%/win64/%exe.vlc%
 set arg.vlc=/S
 set chk.vlc=%ProgramFiles%\VideoLAN\VLC\vlc.exe
+set regtext.vlc=VLC media player
+set regsearch.vlc=%uninstallreg64%
 set reg.vlc=VLC media player
-set reg.vlc=%uninstallreg64%\VLC media player
 
 set pkgver.joplin=2.7.15
 set ver.joplin=%pkgver.joplin%
@@ -70,7 +76,8 @@ set url.joplin=https://github.com/laurent22/joplin/releases/download/v%pkgver.jo
 set arg.joplin=/ALLUSERS=1 /S
 set chk.joplin=%ProgramFiles%\Joplin\Joplin.exe
 set regtext.joplin=Joplin
-set reg.joplin=%uninstallreg64%\499c114e-8890-5040-9c02-24abe7d3ebe9
+set regsearch.joplin=%uninstallreg64%
+set reg.joplin=499c114e-8890-5040-9c02-24abe7d3ebe9
 
 set pkgver.bbit=4.4.2
 set ver.bbit=4.4.2.2142
@@ -80,7 +87,8 @@ set url.bbit=https://download.bleachbit.org/%exe.bbit%
 set arg.bbit=/S /allusers
 set chk.bbit=%ProgramFiles(x86)%\BleachBit\bleachbit.exe
 set regtext.bbit=BleachBit
-set reg.bbit=%uninstallreg32%\BleachBit
+set regsearch.bbit=%uninstallreg32%
+set reg.bbit=BleachBit
 
 set pkgver.7zip=2107
 set ver.7zip=21.07.00.0
@@ -90,7 +98,8 @@ set url.7zip=https://www.7-zip.org/a/%exe.7zip%
 set arg.7zip=/qn /norestart
 set chk.7zip=%ProgramFiles%\7-Zip\7z.exe
 set regtext.7zip=7-Zip
-set reg.7zip=%uninstallreg64%\{23170F69-40C1-2702-2107-000001000000}
+set regsearch.7zip=%uninstallreg64%
+set reg.7zip={23170F69-40C1-2702-2107-000001000000}
 
 set pkgver.spdf=3.4.1
 set ver.spdf=%pkgver.spdf%
@@ -100,7 +109,8 @@ set url.spdf=https://files.sumatrapdfreader.org/file/kjk-files/software/sumatrap
 set arg.spdf=/S
 set chk.spdf=%LOCALAPPDATA%\SumatraPDF\SumatraPDF.exe
 set regtext.spdf=Sumatra
-set reg.spdf=%uninstallreg64%\SumatraPDF
+set regsearch.spdf=%uninstallreg64%
+set reg.spdf=SumatraPDF
 
 set pkgver.tpaint=0.9.27-3
 set ver.tpaint=%pkgver.tpaint%
@@ -110,7 +120,8 @@ set url.tpaint=https://sourceforge.net/projects/tuxpaint/files/tuxpaint/0.9.27/%
 set arg.tpaint=/VERYSILENT /NORESTART
 set chk.tpaint=%ProgramFiles(x86)%\TuxPaint\tuxpaint.exe
 set regtext.tpaint=Tux Paint
-set reg.tpaint=%uninstallreg32%\Tux Paint_is1
+set regsearch.tpaint=%uninstallreg32%
+set reg.tpaint=Tux Paint_is1
 
 set pkgver.tstamps=2021-11-25-2
 set ver.tstamps=%pkgver.tstamps%
@@ -120,7 +131,8 @@ set url.tstamps=https://sourceforge.net/projects/tuxpaint/files/tuxpaint-stamps/
 set arg.tstamps=/VERYSILENT /NORESTART
 set chk.tstamps=%ProgramFiles(x86)%\TuxPaint\tuxpaint.exe
 set regtext.tstamps=Tux Paint Stamps
-set reg.tstamps=%uninstallreg32%\Tux Paint Stamps_is1
+set regsearch.tstamps=%uninstallreg32%
+set reg.tstamps=Tux Paint Stamps_is1
 
 set pkgver.puzzlecollection=20220128.c43a34f
 set ver.puzzlecollection=0.0.13020.0
@@ -130,4 +142,5 @@ set url.puzzlecollection=https://www.chiark.greenend.org.uk/~sgtatham/puzzles/%e
 set arg.puzzlecollection=/qn /norestart
 set chk.puzzlecollection=%ProgramFiles%\Simon Tatham's Portable Puzzle Collection\solo.exe
 set regtext.puzzlecollection=Simon Tatham's Portable Puzzle Collection
-set reg.puzzlecollection=%uninstallreg64%\{F0EC548C-2799-4BD1-864A-4C76E2022E52}
+set regsearch.puzzlecollection=%uninstallreg64%
+set reg.puzzlecollection={F0EC548C-2799-4BD1-864A-4C76E2022E52}
